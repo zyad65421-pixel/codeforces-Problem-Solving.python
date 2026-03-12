1ae473a67e870f90b350077f7ea94c9136ee7254
Ziad Mohamed Abdel Nasser <237123684+zyad65421-pixel@users.noreply.github.com>
2026-03-12 15:54:30 +0200

    feat: Level-C Mastery 🧠 🛡️ 💎 ⚡
    🎯 Codeforces Journey 🚀 🐍 Leveling up A to C ✨ 🧠 Logic & Efficiency 🛡️ 💎

---
+++ Level-C/Safe Division.py
@@ -0,0 +1,9 @@
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    result = num1 / num2
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero!")
